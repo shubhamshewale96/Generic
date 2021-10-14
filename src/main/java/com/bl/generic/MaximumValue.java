0a159d420330e92bbc0dca556bbb0e9f67e1afa3
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-14 18:38:44 +0530

    add:find maximum string value

--- src/main/java/com/bl/generic/MaximumValue.java
+++ src/main/java/com/bl/generic/MaximumValue.java
@@ -2,8 +2,8 @@ package com.bl.generic;
 
 public class MaximumValue {
 	/**
-     * Find Integer maximum value
-     */
+	 * Find Integer maximum value
+	 */
 	public Integer findMax(Integer value1, Integer value2, Integer value3) {
 		Integer max = value1;
 		if (value2.compareTo(max) > 0) {
@@ -15,5 +15,34 @@ public class MaximumValue {
 		System.out.println("Maximum Integer number is " + max);
 		return max;
 	}
-	
+
+	/**
+	 * Find float maximum value
+	 */
+	public Float findMax(Float value1, Float value2, Float value3) {
+		// TODO Auto-generated method stub
+		Float max = value1;
+		if (value2.compareTo(max) > 0) {
+			max = value2;
+		}
+		if (value3.compareTo(max) > 0) {
+			max = value3;
+		}
+		System.out.println("Maximum Integer number is " + max);
+		return max;
+	}
+
+	public String findMax(String value1, String value2, String value3) {
+		// TODO Auto-generated method stub
+		String max = value1;
+		if (value2.compareTo(max) > 0) {
+			max = value2;
+		}
+		if (value3.compareTo(max) > 0) {
+			max = value3;
+		}
+		System.out.println("Maximum Integer number is " + max);
+		return max;
+	}
+
 }
